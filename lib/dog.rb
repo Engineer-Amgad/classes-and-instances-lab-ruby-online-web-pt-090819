470ec2fd3b8eef22e52808ae036ea88e3b6df187
Amgad Malik <amgadmalik@yahoo.com>
2019-09-28 14:39:50 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog 
+
+end 
+
+fido = Dog.New 
+end
+
+snoopy = Dog.New 
+end
+
+lassie = Dog.New 
+end
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+end
+
+adele_goldberg = Person.New 
+end 
+
+alan_key = Person.New
